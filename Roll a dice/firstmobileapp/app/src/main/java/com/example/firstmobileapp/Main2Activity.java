161be8e43aa83f54fe2673f2efebc6e7a14b7170
kareem saeed <karimsaid026@gmail.com>
2020-04-26 02:45:17 +0200

    the History & main layouts Edited

--- Roll a dice/firstmobileapp/app/src/main/java/com/example/firstmobileapp/Main2Activity.java
+++ Roll a dice/firstmobileapp/app/src/main/java/com/example/firstmobileapp/Main2Activity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,35 +31,36 @@ public class Main2Activity extends AppCompatActivity {
             TextView txt10 = (TextView) findViewById(R.id.text10);
 
 
+
             String x1 = getIntent().getStringExtra("key1");
-            if (x1 != null) txt1.setText(x1);
+            if (x1 != null){ txt1.setText(x1); txt1.setGravity(Gravity.CENTER);}
 
             String x2 = getIntent().getStringExtra("key2");
-            if (x2 != null) txt2.setText(x2);
+            if (x2 != null) {txt2.setText(x2); txt2.setGravity(Gravity.CENTER);}
 
             String x3 = getIntent().getStringExtra("key3");
-            if (x3 != null) txt3.setText(x3);
+            if (x3 != null) {txt3.setText(x3); txt3.setGravity(Gravity.CENTER);}
 
             String x4 = getIntent().getStringExtra("key4");
-            if (x4 != null) txt4.setText(x4);
+            if (x4 != null) {txt4.setText(x4); txt4.setGravity(Gravity.CENTER);}
 
             String x5 = getIntent().getStringExtra("key5");
-            if (x5 != null) txt5.setText(x5);
+            if (x5 != null) {txt5.setText(x5); txt5.setGravity(Gravity.CENTER);}
 
             String x6 = getIntent().getStringExtra("key6");
-            if (x6 != null) txt6.setText(x6);
+            if (x6 != null) {txt6.setText(x6); txt6.setGravity(Gravity.CENTER);}
 
             String x7 = getIntent().getStringExtra("key7");
-            if (x7 != null) txt7.setText(x7);
+            if (x7 != null) {txt7.setText(x7); txt7.setGravity(Gravity.CENTER);}
 
             String x8 = getIntent().getStringExtra("key8");
-            if (x8 != null) txt8.setText(x8);
+            if (x8 != null) {txt8.setText(x8); txt8.setGravity(Gravity.CENTER);}
 
             String x9 = getIntent().getStringExtra("key9");
-            if (x9 != null) txt9.setText(x9);
+            if (x9 != null) {txt9.setText(x9); txt9.setGravity(Gravity.CENTER);}
 
             String x10 = getIntent().getStringExtra("key10");
-            if (x10 != null) txt10.setText(x10);
+            if (x10 != null) {txt10.setText(x10); txt10.setGravity(Gravity.CENTER);}
         }
 
     }
